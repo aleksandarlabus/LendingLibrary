57cd3c99f4e9f6e525e27c3e5a50079121beeab5
Aleksandar Labus <aleksandar.labus@azus.local>
2016-07-27 15:29:01 +0200

    After More jdbc

--- src/ui/DatabaseConnectionTesting.java
+++ src/ui/DatabaseConnectionTesting.java
@@ -1,31 +1,86 @@
-
 package ui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnectionTesting {
+
     public static void main(String[] args) {
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/library", "root", "aca1984");
-            
-            Statement stm = conn.createStatement();
-            
-            ResultSet rs = stm.executeQuery("select * from materials");
-            
-            //do something here
-            
-            rs.close();
-            stm.close();
-            conn.close();
-            
-        }catch (Exception e){
+        RetriveResultSet("select * from materials");    
+        ChangeData("update materials set title = ? where id = ?", "A Second test", 1);
+        RetriveResultSet("select * from materials");
+    }
+    
+    public static void ChangeData(String sql, String title, int id){
+        Connection conn = null;
+        PreparedStatement stm = null;
+        
+        try {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/library", "root", "aca1984");
+
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, title);
+                stm.setInt(2, id);
+                
+                int results = stm.executeUpdate();
+                
+                System.out.println("Records amended" + results);
+   
+            } finally {
+                if(stm != null) stm.close();
+                if(conn != null) conn.close();
+            }
+
+        } catch (ClassNotFoundException e) {
             System.out.println("Something went wrong");
             System.out.println(e);
+        } catch (SQLException e) {
+            System.out.println("Something went wrong");
+            System.out.println(e);
+
+        }
+    
+    }
+    
+    public static void RetriveResultSet(String sql) {
+        Connection conn = null;
+        Statement stm = null;
+        ResultSet rs = null;
+        try {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/library", "root", "aca1984");
+
+                stm = conn.createStatement();
+
+                rs = stm.executeQuery(sql);
+
+                while (rs.next()) {
+                    System.out.println(rs.getString("title"));
+                }
+            } finally {
+                if(rs!= null) rs.close();
+                if(stm != null) stm.close();
+                if(conn != null) conn.close();
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Something went wrong");
+            System.out.println(e);
+        } catch (SQLException e) {
+            System.out.println("Something went wrong");
+            System.out.println(e);
+
         }
     }
 }
